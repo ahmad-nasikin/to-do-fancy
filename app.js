9f002b5042791784dc6bef3379a385a3c9720fb6
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-23 05:02:27 +0700

    setup app dan install modul

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/to-do-fancy-nasikin');
+
+const index = require('./routes/index')
+
+var app = express()
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/', index)
+
+app.listen(3000, () => {
+    console.log('Listening port 3000')
+})
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+var userSchema = new Schema({
+
+})
+
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
