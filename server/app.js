2629ae4e410992189d6d8d6241f53d71d2986bf2
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-27 10:28:40 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/to-do-fancy-nasikin');
+// mongoose.connect('mongodb://localhost/to-do-fancy-nasikin');
+mongoose.connect('mongodb://to-do-fancy:to-do-fancy@cluster0-shard-00-00-wtrnn.mongodb.net:27017,cluster0-shard-00-01-wtrnn.mongodb.net:27017,cluster0-shard-00-02-wtrnn.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 const index = require('./routes/index')
 const todo = require('./routes/todo')
